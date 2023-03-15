577bc987d05c100c5e0288d51185b6bbde57aa03
Cesar Cardoso de Oliveira <96243515+ccoliveira91@users.noreply.github.com>
2023-03-15 17:44:42 -0300

    Update index.tsx

--- src/pages/CompleteOrder/components/PaymentInput/index.tsx
+++ src/pages/CompleteOrder/components/PaymentInput/index.tsx
@@ -1,7 +1,10 @@
 import { CreditCard } from 'phosphor-react';
+import { useContext } from 'react';
+import { CartContext } from '../../../../contexts/CartContext';
 import { PaymentInputContainer } from './styles';
 
 export function PaymentInput() {
+
   return (
     <PaymentInputContainer>
       <CreditCard size={16} />
