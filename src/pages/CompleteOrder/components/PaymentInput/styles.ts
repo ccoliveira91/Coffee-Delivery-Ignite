1606d619c66930c91da9e57b39d5f911442343ce
Cesar Cardoso de Oliveira <96243515+ccoliveira91@users.noreply.github.com>
2023-03-16 17:58:45 -0300

    Update styles.ts

--- src/pages/CompleteOrder/components/PaymentInput/styles.ts
+++ src/pages/CompleteOrder/components/PaymentInput/styles.ts
@@ -1,6 +1,21 @@
 import styled from 'styled-components';
 
 export const PaymentInputContainer = styled.div`
+  input {
+    visibility: hidden;
+    appearance: none;
+  }
+
+  input:checked + label div {
+    background: ${(props) => props.theme.colors['brand-purple-light']};
+    border-color: ${(props) => props.theme.colors['brand-purple']};
+    &:hover {
+      background: ${(props) => props.theme.colors['brand-purple-light']};
+    }
+  }
+`;
+
+export const ContentContainer = styled.div`
   padding: 0 1rem;
   background: ${(props) => props.theme.colors['base-button']};
   color: ${(props) => props.theme.colors['base-text']};
@@ -9,19 +24,16 @@ export const PaymentInputContainer = styled.div`
   justify-content: flex-start;
   gap: 0.75rem;
   font-size: 0.75rem;
+  text-transform: uppercase;
   border-radius: 6px;
   height: 3rem;
   border: 1px solid ${(props) => props.theme.colors['base-button']};
   transition: 0.4s;
-  text-transform: uppercase;
-
   svg {
     color: ${(props) => props.theme.colors['brand-purple']};
   }
-
   &:hover {
     background: ${(props) => props.theme.colors['base-hover']};
   }
-
   user-select: none;
 `;
