78987c2ffb027eb763e910f4203a78e0ebcd8dfd
Cesar Cardoso de Oliveira <96243515+ccoliveira91@users.noreply.github.com>
2023-03-16 17:58:54 -0300

    Update PaymentOptions.tsx

--- src/pages/CompleteOrder/components/CompleteOrderForm/PaymentOptions.tsx
+++ src/pages/CompleteOrder/components/CompleteOrderForm/PaymentOptions.tsx
@@ -1,13 +1,48 @@
-import { Button } from '../../../../components/Button';
+import { Bank, CreditCard, Money } from 'phosphor-react';
+import { useFormContext } from 'react-hook-form';
 import { PaymentInput } from '../PaymentInput';
 import { PaymentOptionsContainer } from './styles';
 
-export function PaymentOptions() {
+export const paymentMethod = {
+  credit: {
+    id: 1,
+    method: 'Cartão de Crédito',
+    icon: <CreditCard size={16} />,
+  },
+  debit: {
+    id: 2,
+    method: 'Cartão de Débito',
+    icon: <Bank size={16} />,
+  },
+  money: {
+    id: 3,
+    method: 'Dinheiro',
+    icon: <Money size={16} />,
+  },
+};
+
+
+export function PaymentOptions() {  
+  const {
+    register,
+    formState: { errors },
+  } = useFormContext();
+
+  // const paymentMethodError = errors?.paymentMethod
+  //   ?.message as unknown as string;
+
   return (
     <PaymentOptionsContainer>
-      <PaymentInput />
-      <PaymentInput />
-      <PaymentInput />
+      {Object.entries(paymentMethod).map(([key, { id, method, icon }]) => (
+        <PaymentInput
+          key={id}
+          id={key}
+          icon={icon}
+          label={method}
+          value={key}
+          {...register('paymentMethod')}
+        />
+      ))}
     </PaymentOptionsContainer>
   );
 }
