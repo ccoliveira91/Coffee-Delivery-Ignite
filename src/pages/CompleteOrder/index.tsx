064e3c9c17f2097bee16bf6298f85c57731babbd
Cesar Cardoso de Oliveira <96243515+ccoliveira91@users.noreply.github.com>
2023-03-17 18:02:25 -0300

    Update index.tsx

--- src/pages/CompleteOrder/index.tsx
+++ src/pages/CompleteOrder/index.tsx
@@ -4,17 +4,32 @@ import { CompleteOrderContainer } from './styles';
 import { z } from 'zod';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { FormProvider, useForm } from 'react-hook-form';
+import { useNavigate } from 'react-router-dom';
+import { useContext } from 'react';
+import { CartContext } from '../../contexts/CartContext';
+
+enum PaymentMethods {
+  credit = 'credit',
+  debit = 'debit',
+  money = 'money',
+}
 
 const CompleteOrderFormValidationSchema = z.object({
   cep: z
     .string()
-    .min(8, { message: 'Informe o CEP, deve conter 8 dígitos' })
+    .min(8, { message: 'Informe o CEP, ele deve conter 8 dígitos' })
     .max(8, { message: 'Maxímo de 8 dígitos' }),
-  street: z.string().min(1, {message: 'Informe o nome da rua'}),
-  number: z.string().min(1, {message: 'Informe o número'}),
-  neighborhood: z.string().min(1, {message: 'Informe o nome do bairro'}),
-  city: z.string().min(1, {message: 'Informe o nome da cidade'}),
-  uf: z.string().min(1, {message: 'Informe o nome do estado'}),
+  rua: z.string().min(1, { message: 'Informe o nome da rua' }),
+  numero: z.string().min(1, { message: 'Informe o número' }),
+  complemento: z.string(),
+  bairro: z.string().min(1, { message: 'Informe o nome do bairro' }),
+  cidade: z.string().min(1, { message: 'Informe o nome da cidade' }),
+  uf: z.string().min(1, { message: 'Informe a UF' }),
+  paymentMethod: z.nativeEnum(PaymentMethods, {
+    errorMap: () => {
+      return { message: 'Informe um método de pagamento' };
+    },
+  }),
 });
 
 export type OrderData = z.infer<typeof CompleteOrderFormValidationSchema>;
@@ -22,14 +37,27 @@ export type OrderData = z.infer<typeof CompleteOrderFormValidationSchema>;
 type CompleteOrderFormData = OrderData;
 
 export function CompleteOrder() {
+  function useCart() {
+    const context = useContext(CartContext);
+    return context;
+  }
+
   const completeOrderForm = useForm<CompleteOrderFormData>({
     resolver: zodResolver(CompleteOrderFormValidationSchema),
+    defaultValues: {
+      paymentMethod: undefined,
+    },
   });
 
   const { handleSubmit } = completeOrderForm;
+  const navigate = useNavigate();
+  const { cleanOrder } = useCart();
 
   function handleCompleteOrder(data: CompleteOrderFormData) {
-    console.log(data);
+    navigate('/confirmedOrder', {
+      state: data,
+    });
+    cleanOrder();
   }
 
   return (
