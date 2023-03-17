b6517f28e443a55f06abc53ae69ef53d2b628edf
Cesar Cardoso de Oliveira <96243515+ccoliveira91@users.noreply.github.com>
2023-03-17 18:02:31 -0300

    Update AddressForm.tsx

--- src/pages/CompleteOrder/components/CompleteOrderForm/AddressForm.tsx
+++ src/pages/CompleteOrder/components/CompleteOrderForm/AddressForm.tsx
@@ -1,54 +1,32 @@
-import { useForm } from 'react-hook-form';
+import { useFormContext } from 'react-hook-form';
 import { Input } from '../../../../components/Input';
 import { AddressFormContainer } from './styles';
-import * as zod from 'zod';
-import { zodResolver } from '@hookform/resolvers/zod';
-import { useEffect } from 'react';
 
-const addressFormValidationSchema = zod.object({
-  cep: zod.string().min(8, 'CEP deve conter 8 dígitos').max(8),
-  rua: zod.string().min(1),
-  numero: zod.string().min(1),
-  complemento: zod.string(),
-  bairro: zod.string().min(1),
-  cidade: zod.string().min(1),
-  uf: zod.string().min(1).max(2),
-});
-
-type NewAddressFormData = zod.infer<typeof addressFormValidationSchema>;
+interface ErrorsType {
+  errors: {
+    [key: string]: {
+      message: string;
+    };
+  };
+}
 
 export function AddressForm() {
-  const addressForm = useForm<NewAddressFormData>({
-    resolver: zodResolver(addressFormValidationSchema),
-    defaultValues: {
-      cep: '',
-      rua: '',
-      numero: '',
-      complemento:'',
-      bairro: '',
-      cidade: '',
-      uf: '',
-    },
-    mode: 'onChange',
-  });
-
-  const {
-    register,
-    reset,
-    formState,
-  } = addressForm;
-
-  console.log(reset)
+  const { register, formState } = useFormContext();
+  const { errors } = formState as unknown as ErrorsType;
 
   return (
     <AddressFormContainer>
-      <Input placeholder="CEP" className="cep" {...register('cep')} />
-      <Input placeholder="Rua" className="street" {...register('rua')} />
-      <Input placeholder="Número" type="number" {...register('numero')} />
-      <Input placeholder="Complemento" className="complement" {...register('complemento')} />
-      <Input placeholder="Bairro" {...register('bairro')} />
-      <Input placeholder="Cidade" {...register('cidade')} />
-      <Input placeholder="UF" {...register('uf')} />
+      <Input placeholder="CEP" className="cep" {...register('cep')} error={errors.cep?.message} />
+      <Input placeholder="Rua" className="street" {...register('rua')} error={errors.rua?.message}/>
+      <Input placeholder="Número" type="number" {...register('numero')} error={errors.numero?.message}/>
+      <Input
+        placeholder="Complemento"
+        className="complement"
+        {...register('complemento')}
+      />
+      <Input placeholder="Bairro" {...register('bairro')} error={errors.bairro?.message} />
+      <Input placeholder="Cidade" {...register('cidade')} error={errors.cidade?.message} />
+      <Input placeholder="UF" {...register('uf')} error={errors.uf?.message} />
     </AddressFormContainer>
   );
 }
